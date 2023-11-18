04c8c2aa6d9db629292fc5ded2284e75d50f7026
Akshay Bhasin <107040611+Akshay9704@users.noreply.github.com>
2023-11-18 13:26:04 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Features from "./components/features";
+import Testimonials from "./components/testimonials";
+import Banner from "./components/banner";
+import Footer from "./components/footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Features/>
+      <Testimonials/>
+      <Banner/>
+      <Footer/>
+    </>
   );
 }
 
